376f482c6cd820b94efeb903260f0adda8176387
ume-aiman <136246835+ume-aiman@users.noreply.github.com>
2024-04-25 22:10:11 +0500

    Add files via upload

---
+++ week 7/index.js
@@ -0,0 +1,8 @@
+//const myp1 = document.getElementByTagName("mydiv");
+//console.log(myp1);
+//myp1 [0].innerhtml ="Hello World";
+const button = document.createElement("button");
+button.onclick = function(){
+    const add = document.querySelectorAll("div");
+    console.log(add);
+}
